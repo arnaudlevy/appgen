5c97c04f3cf5fba2fba55732aeb26968af65277c
Arnaud Levy <arnaud@semiodesign.com>
2017-07-18 10:18:44 +0200

    cli

--- lib/appgen.rb
+++ lib/appgen.rb
@@ -1,12 +1,4 @@
 require 'appgen/version'
-require 'thor'
 
 module Appgen
-  class CLI < Thor
-
-    desc "appgen", "Generates app from description"
-    def appgen
-
-    end
-  end
 end
---
+++ lib/appgen/cli.rb
@@ -0,0 +1,11 @@
+require 'thor'
+
+module Appgen
+  class CLI < Thor
+
+    desc "appgen", "Generates app from description"
+    def appgen
+      puts 'ok'
+    end
+  end
+end
